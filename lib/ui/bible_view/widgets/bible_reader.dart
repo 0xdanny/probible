38d926fdffdbe75160247625973e36906467a679
0xdanny <danny.hex12@gmail.com>
2022-06-23 22:31:27 +0100

    chore: remove reader inline verses

--- lib/ui/bible_view/widgets/bible_reader.dart
+++ lib/ui/bible_view/widgets/bible_reader.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,98 +13,50 @@ class BibleReader extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     List<Widget> children = [const SizedBox(height: 10)];
-    List<InlineSpan> spans = [];
-    children.add(
-      Container(
-        margin: const EdgeInsets.symmetric(horizontal: 32),
-        child: Text.rich(
-          TextSpan(children: spans),
-          style: Theme.of(context).textTheme.headline5!.copyWith(
-                fontWeight: FontWeight.w400,
-                fontSize: ref
-                        .watch(readerSettingsRepositoryProvider.notifier)
-                        .bodyTextSize *
-                    1.4,
-                height: ref
-                        .watch(readerSettingsRepositoryProvider.notifier)
-                        .bodyTextHeight *
-                    1.1,
-                fontFamily:
-                    ref.watch(readerSettingsRepositoryProvider).typeFace,
-              ),
-        ),
-      ),
-    );
+
     for (int i = 0; i < chapter.verses!.length; i++) {
       var item = chapter.verses![i];
-      // var _isFavoriteVerse = ref
-      //     .watch(studyToolsRepositoryProvider)
-      //     .favoriteVerses
-      //     .where((element) => element.id == item.id)
-      //     .isNotEmpty;
-
-      spans.add(
-        WidgetSpan(
-          child: GestureDetector(
-            onTap: () async {
-              HapticFeedback.mediumImpact();
-              // await showFavoriteVerseBottomSheet(context, item);
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(right: 4.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // if (_isFavoriteVerse)
-                  //   Icon(
-                  //     LineAwesomeIcons.heart_1,
-                  //     size: ref
-                  //             .watch(readerSettingsRepositoryProvider)
-                  //             .verseNumberSize *
-                  //         1.3,
-                  //     color: heartColor(context),
-                  //   ),
-                  Text(
-                    item.verseId.toString(),
-                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontSize: ref
-                                  .watch(readerSettingsRepositoryProvider)
-                                  .verseNumberSize *
-                              1.1,
-                          // height: ref.watch(readerSettingsRepositoryProvider).verseNumberHeight,
-                          fontFamily: ref
+      children.add(
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 32),
+          child: Text.rich(
+            TextSpan(children: [
+              TextSpan(
+                text: item.verseId.toString(),
+                style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: ref
                               .watch(readerSettingsRepositoryProvider)
-                              .typeFace,
-                        ),
-                  ),
-                  // if (!_isFavoriteVerse)
-                  //   const Icon(
-                  //     LineAwesomeIcons.heart_1,
-                  //     size: 10,
-                  //     color: CantonColors.transparent,
-                  //   ),
-                ],
+                              .verseNumberSize *
+                          1.1,
+                      fontFamily:
+                          ref.watch(readerSettingsRepositoryProvider).typeFace,
+                    ),
               ),
-            ),
+              const TextSpan(text: "  "),
+              TextSpan(
+                text: item.text,
+                style: Theme.of(context).textTheme.headline5!.copyWith(
+                      fontWeight: FontWeight.w400,
+                      fontSize: ref
+                              .watch(readerSettingsRepositoryProvider.notifier)
+                              .bodyTextSize *
+                          1.4,
+                      height: ref
+                              .watch(readerSettingsRepositoryProvider.notifier)
+                              .bodyTextHeight *
+                          1.1,
+                      fontFamily:
+                          ref.watch(readerSettingsRepositoryProvider).typeFace,
+                    ),
+              ),
+            ]),
           ),
         ),
       );
 
-      spans.add(
-        TextSpan(
-          text: item.text,
-          recognizer: TapGestureRecognizer()
-            ..onTap = () async {
-              HapticFeedback.mediumImpact();
-              // await showFavoriteVerseBottomSheet(context, item);
-            },
-        ),
-      );
-
       if (!(i == chapter.verses!.length - 1)) {
-        spans.add(const TextSpan(text: ' '));
+        children.add(const SizedBox(height: 12));
       }
     }
 
